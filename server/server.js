0d3096ee51f0b8959943b7befd1ba7f9df211742
cur1ousFranz <xfranjeff@gmail.com>
2023-04-14 22:56:01 +0800

    register order/food routes | middleware

--- server/server.js
+++ server/server.js
@@ -3,6 +3,16 @@ const express = require('express')
 const mongoose= require('mongoose')
 const app = express()
 
+// routes
+const foodRoutes = require('./routes/foods')
+const orderRoutes = require('./routes/orders')
+
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log(req.method)
+    next()
+})
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
@@ -12,3 +22,6 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((err) => {
         console.log(err)
     })
+
+app.use(foodRoutes)
+app.use(orderRoutes)
