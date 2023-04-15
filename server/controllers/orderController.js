414b88d6e93cfff337f3e3e7a93c236994c20dcf
cur1ousFranz <xfranjeff@gmail.com>
2023-04-15 22:30:04 +0800

    order controller | read/store functions

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,20 +1,62 @@
+require('dotenv').config()
 const { isValidObjectId } = require('mongoose')
 const Order = require('../models/orderModel')
+const Customer = require('../models/customerModel')
+const extractUserId = require('../auth/auth')
 
 const getAllOrders = async (req, res) => {
+    const user_id = extractUserId(req)
+    const customer = await Customer.findOne({ user_id }).select('_id')
 
-    res.status(200).json({ })
+    const orders = await Order.find({ customer_id: customer._id })
+    res.status(200).json(orders)
 }
 
 const storeOrder = async (req, res) => {
+    const { foods, total_amount, payment_type } = req.body
+    const user_id = extractUserId(req)
+    const customer = await Customer.findOne({ user_id }).select('_id')
+
+    const errorFields = { error : "Pleae fill in all fields"}
+
+    if(!foods) {
+        return res.status(400).json(errorFields)
+    }
+    if(!total_amount) {
+        return res.status(400).json(errorFields)
+    }
+    if(!payment_type) {
+        return res.status(400).json(errorFields)
+    }
     
-    res.status(200).json({})
+    try {
+        const order = await Order.create({ 
+            customer_id: customer._id, 
+            foods,
+            total_amount, 
+            payment_type })
+            
+        res.status(200).json(order)
+
+    } catch (error) {
+        res.status(500).json({ error : error.message })
+    }
 }
 
 const getOrderDetails = async (req, res) => {
-    
+    const { id } = req.params
 
-    res.status(200).json({})
+    if(!isValidObjectId(id)) {
+        return res.status(404).json({ error : "No such order"})
+    }
+
+    const order = await Order.findById({ _id: id})
+
+    if(!order) {
+        return res.status(400).json({ error : "No such order"})
+    }
+
+    res.status(200).json(order)
 }
 
 module.exports = {
