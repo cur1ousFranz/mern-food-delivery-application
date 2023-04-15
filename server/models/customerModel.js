163abbde0cded2ec293cd3940040c4d6a775675c
cur1ousFranz <xfranjeff@gmail.com>
2023-04-15 22:26:39 +0800

    implement customer model/schema

---
+++ server/models/customerModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const customerSchema = new Schema({
+    user_id : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+    },
+    contact_number: {
+        type: String,
+    },
+    address: {
+        type: String,
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Customer', customerSchema)
