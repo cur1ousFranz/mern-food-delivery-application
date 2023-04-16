e24dcf0617193655c431da772aa1d9ded1685f99
cur1ousFranz <xfranjeff@gmail.com>
2023-04-16 22:58:43 +0800

    init category breadcrumbs

---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,21 @@
+const Categories = () => {
+    return ( 
+        <div className="text-sm flex flex-wrap space-x-2 mt-2 md:text-base">
+            <p>All</p>
+            <p className="text-gray-400">/</p>
+            <p>Breakfast</p>
+            <p className="text-gray-400">/</p>
+            <p>Family Meals</p>
+            <p className="text-gray-400">/</p>
+            <p>Kid Meals</p>
+            <p className="text-gray-400">/</p>
+            <p>Burgers</p>
+            <p className="text-gray-400">/</p>
+            <p>Desserts</p>
+            <p className="text-gray-400">/</p>
+            <p>Beverages</p>
+        </div>
+    );
+}
+ 
+export default Categories;
