0e2a6acc6072ec7eddb1bd84371e46e865ca3ca4
cur1ousFranz <xfranjeff@gmail.com>
2023-04-16 23:00:51 +0800

    implement initial routes using browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
+
+import Navbar from "./components/Navbar";
+
+// PAGES
+import Signin from './views/Signin';
+import Home from './views/Home';
+import Signup from './views/Signup';
+import Menu from './views/Menu';
+
 function App() {
+  const { user } = useContext(AuthContext)
+  
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={ <Home/> }/>
+          <Route path='/signin' element={ !user ? <Signin /> : <Navigate to={'/'} /> }/>
+          <Route path='/signup' element={ !user ? <Signup /> : <Navigate to={'/'} /> }/>
+          <Route path='/menu' element={ <Menu />}/>
+        </Routes>
+        
+      </BrowserRouter>
     </div>
   );
 }
