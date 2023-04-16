e983045346deae1dae88df799af704b245e37756
cur1ousFranz <xfranjeff@gmail.com>
2023-04-16 22:59:33 +0800

    include auth/food provider for context api

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AuthContextProvider from './context/AuthContext';
+import FoodContextProvider from './context/FoodContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <FoodContextProvider>
+        <App />
+      </FoodContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
