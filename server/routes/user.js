a33a8b509607e0a738c186b2948174fd4651da7a
cur1ousFranz <xfranjeff@gmail.com>
2023-04-17 22:31:56 +0800

    remove signup function in user controller/routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-const { signupUser, loginUser } = require('../controllers/userController')
+const { loginUser } = require('../controllers/userController')
 
-router.post('/api/users/signup', signupUser)
 router.post('/api/users/login', loginUser)
 
 module.exports = router
