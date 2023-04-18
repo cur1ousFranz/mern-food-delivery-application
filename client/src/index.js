e3024564c6e95bcd027dc0a801d81610a26a709a
cur1ousFranz <xfranjeff@gmail.com>
2023-04-18 23:38:50 +0800

    register store context provider api

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import AuthContextProvider from './context/AuthContext';
 import FoodContextProvider from './context/FoodContext';
+import StoreContextProvider from './context/StoreContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <FoodContextProvider>
-        <App />
-      </FoodContextProvider>
+      <StoreContextProvider>
+        <FoodContextProvider>
+          <App />
+        </FoodContextProvider>
+      </StoreContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
