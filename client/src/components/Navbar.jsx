95029f54fa62acadf6384bee435079372abcc206
cur1ousFranz <xfranjeff@gmail.com>
2023-04-18 23:32:27 +0800

    register stores page and remove manu page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,22 +16,22 @@ const Navbar = () => {
     return (
         <div className="py-6 px-6 border-b flex justify-between">
             <Link to={'/'} className="font-semibold text-xl">
-                <img src="kaon.svg" className="w-20 md:w-24 absolute -mt-2" alt="" />
+                <img src="/kaon.svg" className="w-20 md:w-24 absolute -mt-2" alt="" />
             </Link>
             <div className="hidden md:flex space-x-8 mt-1 font-semibold">
                 <NavLink to={'/'} className="hover:underline hover:text-orange-500">Home</NavLink>
-                <NavLink to={'/menu'} className="hover:underline hover:text-orange-500">Menu</NavLink>
+                <NavLink to={'/stores'} className="hover:underline hover:text-orange-500">Stores</NavLink>
                 <h1 className="hover:underline hover:text-orange-500">About</h1>
                 <h1 className="hover:underline hover:text-orange-500">Services</h1>
                 <h1 className="hover:underline hover:text-orange-500">Delivery</h1>
             </div>
             <div className="space-x-4 flex">
-                <img src="cart4.svg" className="inline-block w-6" alt="" />
+                <img src="/cart4.svg" className="inline-block w-6" alt="" />
                 {user && (
                     <div>
                         <img onClick={() => setprofileDropdown(!profileDropdown)} src="person-circle.svg" className="inline-block w-6 cursor-pointer" alt="" />
 
-                        { profileDropdown && (
+                        {profileDropdown && (
                             <div className="origin-top-right absolute right-0 mr-4 mt-2 w-44 z-10 rounded-md shadow-lg">
                                 <div className="rounded-md border shadow-xs text-start px-2 bg-white">
                                     <div className="p-2">
---
+++ client/src/views/Stores.jsx
@@ -0,0 +1,70 @@
+import { useContext, useEffect, useState } from "react";
+import StoreDetails from "../components/StoreDetails";
+import axios from "axios";
+import { StoreContext } from "../context/StoreContext";
+
+const Stores = () => {
+    const { stores, dispatch } = useContext(StoreContext)
+    const [businessTypes, setBusinessTypes] = useState([])
+
+    useEffect(() => {
+
+        const getAllStores = async () => {
+
+            const response = await axios.get('/api/stores')
+            const data = response.data
+
+            if (response.status === 200) {
+                dispatch({ type: 'SET_STORES', payload: data })
+            }
+
+        }
+
+        const getAllBusinessTypes = async () => {
+            const response = await axios.get('/api/business-type')
+            const data = response.data
+
+            if (response.status === 200) {
+                setBusinessTypes(data)
+            }
+        }
+
+        getAllStores()
+        getAllBusinessTypes()
+
+    }, [])
+
+    const getStoresByBusinessType = (businessType) => {
+        return stores.filter(store => store.business_type === businessType);
+    }
+
+    return (
+        <div>
+            {businessTypes && businessTypes.map(type => {
+                const storesByType = getStoresByBusinessType(type.name);
+
+                return (
+                    <div className="px-6 md:px-24 py-8" key={type._id}>
+                        <h1 className="font-semibold text-lg md:text-2xl">{type.name}</h1>
+                        <div className="grid grid-cols-2 gap-4 py-8 md:grid-cols-4">
+
+                            {storesByType.length > 0 && storesByType.map((store) => (
+                                <StoreDetails store={store} key={store._id} />
+                            ))}
+
+                        </div>
+                        {storesByType.length === 0 && (
+                            <div className="text-center shadow-md border bg-gray-200 p-24">
+                                <p className="text-2xl font-bold text-gray-500">No stores at the moment.</p>
+                            </div>
+                        )}
+                    </div>
+                )
+
+            })}
+
+        </div>
+    );
+}
+
+export default Stores;
