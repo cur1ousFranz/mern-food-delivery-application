d869f285af81c8d2fdd99daaaf3884255e0fc560
cur1ousFranz <xfranjeff@gmail.com>
2023-04-18 23:34:56 +0800

    extract store details into separate component

---
+++ client/src/components/StoreDetails.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+const StoreDetails = ({ store }) => {
+    return (
+        <Link to={'/stores/' + store._id} className="border">
+            <img src="store.png" className="object-cover" alt="" style={{ width: "100%", height: "200px" }}/>
+            <div className="px-4 py-2">
+                <p className="font-semibold truncate">{ store.store_name }</p>
+                <p className="text-sm">{ store.contact_number }</p>
+                <p className="text-orange-500">{ store.store_address }</p>
+            </div>
+        </Link>
+    );
+}
+
+export default StoreDetails;
