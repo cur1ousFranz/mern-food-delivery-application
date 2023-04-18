68787a630b481f67d93f4befef9630cbc8a92bb3
cur1ousFranz <xfranjeff@gmail.com>
2023-04-18 16:19:48 +0800

    rename merchant to store for consistency

---
+++ server/routes/store.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const { getAllStores, storeStore, getStoreDetails } = require('../controllers/storeController')
+
+router.get('/api/stores', getAllStores)
+router.post('/api/stores', storeStore)
+router.get('/api/stores/:id', getStoreDetails)
+
+module.exports = router
