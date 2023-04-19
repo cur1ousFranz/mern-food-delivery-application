4af2b2497a7d34a5a973f551903597bcf683aed1
cur1ousFranz <xfranjeff@gmail.com>
2023-04-19 22:52:03 +0800

    include select function props

--- client/src/components/FoodDetails.jsx
+++ client/src/components/FoodDetails.jsx
@@ -1,15 +1,13 @@
-const FoodDetails = ({ food }) => {
+const FoodDetails = ({ food, selectFood }) => {
     return (
         <div className="border rounded-md">
-            <img src="/gray-image.png" className="rounded-t-md object-cover" alt="" 
-            style={{ width: "100%", height: "200px" }}/>
+            <img src="/gray-image.png" className="rounded-t-md h-100 object-cover" alt={food.name} />
             <div className="px-4 py-2">
-                <p className="font-semibold truncate">{food.name}</p>
-                <p className="text-sm">{food.description}</p>
+                <p className="text-sm md:text-base font-semibold truncate">{food.name}</p>
                 <p className="text-orange-500"><span className="text-lg">₱</span> {food.price}</p>
             </div>
-            <button className={food.available ? "text-sm w-full py-2 rounded-b-md bg-orange-500 hover:bg-orange-400 text-white" : "text-sm w-full py-2 rounded-b-md cursor-not-allowed bg-gray-400 text-white"} disabled={!food.available}>
-                { food.available ? 'Order Now' : 'Not available'}
+            <button onClick={() => selectFood(true, food)} className={food.available ? "text-sm w-full py-1 md:py-2 rounded-b-md bg-orange-500 hover:bg-orange-400 text-white" : "text-sm w-full py-2 rounded-b-md cursor-not-allowed bg-gray-400 text-white"} disabled={!food.available}>
+                { food.available ? 'Quick View' : 'Not available'}
             </button>
         </div>
     );
