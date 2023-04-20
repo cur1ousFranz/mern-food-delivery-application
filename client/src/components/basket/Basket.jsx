60226da4f8a977901074c5fedac1ec42c93aa50b
cur1ousFranz <xfranjeff@gmail.com>
2023-04-20 22:48:47 +0800

    extract basket to component | layout/design

---
+++ client/src/components/basket/Basket.jsx
@@ -0,0 +1,48 @@
+import { useContext, useState } from "react";
+import { BasketContext } from "../../context/BasketContext";
+import BasketFood from "./BasketFood";
+
+const Basket = () => {
+    const [basketDropdown, setBasketDropdown] = useState(false)
+    const { basket } = useContext(BasketContext)
+
+    return (
+        <div>
+            <div>
+                <img onClick={() => setBasketDropdown(!basketDropdown)} src="/basket.svg" className="inline-block w-6 cursor-pointer" alt="" />
+                {basket.length > 0 && (
+                    <span className="rounded-full px-2 py-1 text-xs absolute -ms-2 -mt-2 bg-red-500 text-white">
+                        {basket.length}
+                    </span>
+                )}
+            </div>
+
+            {basketDropdown && (
+                <div className="absolute right-0 mr-4 p-2 mt-2 w-96 z-10 rounded-md border bg-white" style={{ minHeight: "300px" }}>
+                    <div className="flex space-x-2">
+                        <span><img src="/basket.svg" className="w-4 inline-block" alt="" /></span>
+                        <h1 className="font-semibold mt-0.5">Basket</h1>
+                    </div>
+                    <div className="overflow-y-auto px-2 max-h-60 ">
+                        {basket && basket.map(food => (
+                            <BasketFood food={food} key={food._id} />
+                        ))}
+
+                        {basket.length === 0 && (
+                            <div className="text-center py-24">
+                                <img src="/basket.svg" className="w-6 mx-auto " alt="" />
+                                <p className="font-semibold text-gray-700">Basket is empty.</p>
+                            </div>
+                        )}
+                    </div>
+                    <button disabled={basket.length === 0} className={basket.length === 0 ? "absolute bottom-0 w-full py-2 right-0 uppercase rounded-sm bg-gray-400" : "absolute bottom-0 w-full py-2 right-0 uppercase rounded-sm bg-orange-500 hover:bg-orange-600 text-white"}>
+                        Checkout {basket.length > 0 ? `(${basket.length})` : ''}
+                    </button>
+                </div>
+            )}
+
+        </div>
+    );
+}
+
+export default Basket;
