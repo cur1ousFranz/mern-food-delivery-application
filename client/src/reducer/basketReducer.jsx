194ba352b07d242a05fc16805f8b63a379aed9d7
cur1ousFranz <xfranjeff@gmail.com>
2023-04-21 22:47:03 +0800

    if food exist in basket then update its quantity

--- client/src/reducer/basketReducer.jsx
+++ client/src/reducer/basketReducer.jsx
@@ -1,21 +1,35 @@
 const basketReducer = (state, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case 'SET_BASKET':
-            return { 
+            return {
                 basket: action.payload
             }
         case 'ADD_TO_BASKET':
-            return { 
-                basket: [...state.basket, action.payload]
-            }
-        case 'REMOVE_TO_BASKET':
-            return { basket: state.basket.filter((food) => food._id !== action.payload.id)}
-        case 'SET_FOOD_COUNT':
+            const index = state.basket.findIndex(food => food._id === action.payload._id)
 
-            return { 
-                basket : state.basket.filter(food => {
-                    if(food._id === action.payload.id){
-                        food.foodCount = action.payload.foodCount
+            if(index === -1){
+                return {
+                    basket: [...state.basket, action.payload]
+                }
+            }
+
+            return {
+                basket: state.basket.map(food => {
+                    if(food._id === action.payload._id){
+                        return {...food, foodQuantity: food.foodQuantity + action.payload.foodQuantity}
+                    }
+
+                    return food
+                })
+            }
+
+        case 'REMOVE_TO_BASKET':
+            return { basket: state.basket.filter((food) => food._id !== action.payload.id) }
+        case 'SET_FOOD_QUANTITY':
+            return {
+                basket: state.basket.filter(food => {
+                    if (food._id === action.payload.id) {
+                        food.foodQuantity = action.payload.foodQuantity
                     }
 
                     return food
@@ -27,4 +41,3 @@ const basketReducer = (state, action) => {
 }
 
 export default basketReducer
- 
