258515a44cae122602c88b107e291a85081d53f3
cur1ousFranz <xfranjeff@gmail.com>
2023-04-21 22:48:21 +0800

    format price

--- client/src/components/foods/FoodDetails.jsx
+++ client/src/components/foods/FoodDetails.jsx
@@ -4,7 +4,7 @@ const FoodDetails = ({ food, selectFood }) => {
             <img src="/gray-image.png" className="rounded-t-md h-100 object-cover" alt={food.name} />
             <div className="px-4 py-2">
                 <p className="text-sm md:text-base font-semibold truncate">{food.name}</p>
-                <p className="text-orange-500"><span className="text-lg">₱</span> {food.price}</p>
+                <p className="text-orange-500"><span className="text-lg">₱</span> {food.price.toLocaleString()}</p>
             </div>
             <button onClick={() => selectFood(true, food)} className={food.available ? "text-sm w-full py-1 md:py-2 rounded-b-md bg-orange-500 hover:bg-orange-400 text-white" : "text-sm w-full py-2 rounded-b-md cursor-not-allowed bg-gray-400 text-white"} disabled={!food.available}>
                 { food.available ? 'Quick View' : 'Not available'}
