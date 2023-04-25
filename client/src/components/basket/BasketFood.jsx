4907315c14df0312a66acbacb7c843b8d020ee6b
cur1ousFranz <xfranjeff@gmail.com>
2023-04-25 23:26:50 +0800

    replace food.id to uuid property

--- client/src/components/basket/BasketFood.jsx
+++ client/src/components/basket/BasketFood.jsx
@@ -16,32 +16,41 @@ const BasketFood = ({ food }) => {
     }
 
     const removeFood = () => {
-        dispatch({ type: 'REMOVE_TO_BASKET', payload: { id: food._id } })
+        dispatch({ type: 'REMOVE_TO_BASKET', payload: { uuid: food.uuid } })
     }
 
     useEffect(() => {
-        const index = basket.findIndex(f => f._id === food._id)
+        const index = basket.findIndex(f => f.uuid === food.uuid)
         setFoodQuantity(basket[index].foodQuantity)
-        
+
     }, [basket])
 
     useEffect(() => {
-        dispatch({ type: 'SET_FOOD_QUANTITY', payload: { id: food._id, foodQuantity } })
+        dispatch({ type: 'SET_FOOD_QUANTITY', payload: { uuid: food.uuid, foodQuantity } })
 
     }, [foodQuantity])
 
     return (
         <div className="p-2 w-full">
-            <p className="text-sm">{food.name}</p>
+            <p className="font-semibold">{food.name}</p>
             <div className="flex space-x-12 w-full">
                 <div className="w-1/2">
-                    <img src="/gray-image.png" className="rounded-md" alt="" />
+                    <img
+                        src="/food_image.png"
+                        className="rounded-md object-cover"
+                        alt=""
+                        style={{ minHeight: "7rem", maxHeight: "7rem" }} />
                 </div>
                 <div className="flex-row">
                     <div className="flex h-fit">
-                        <button onClick={() => handleFoodQuantity('-')} disabled={foodQuantity === 1} className="px-2 border text-sm">-</button>
+                        <button
+                            onClick={() => handleFoodQuantity('-')}
+                            disabled={foodQuantity === 1}
+                            className="px-2 border text-sm">-</button>
                         <p className="px-2 border text-sm">{foodQuantity}</p>
-                        <button onClick={() => handleFoodQuantity('+')} className="px-2 border text-sm">+</button>
+                        <button
+                            onClick={() => handleFoodQuantity('+')}
+                            className="px-2 border text-sm">+</button>
                     </div>
                     <p className="mt-3 text-center">
                         <span className="text-lg text-orange-500">₱ </span>
