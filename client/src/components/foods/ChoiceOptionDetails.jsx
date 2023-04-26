72bf121f3b5069ddd3677d97e19ff30e5b4f46cb
cur1ousFranz <xfranjeff@gmail.com>
2023-04-26 22:48:18 +0800

    disable checkboxes if maximum select_count is reached

--- client/src/components/foods/ChoiceOptionDetails.jsx
+++ client/src/components/foods/ChoiceOptionDetails.jsx
@@ -1,4 +1,15 @@
-const ChoiceOptionDetails = ({ choice, option, handleRadioOption, handleCheckboxOption, index }) => {
+const ChoiceOptionDetails = ({
+    choice,
+    option,
+    handleRadioOption,
+    handleCheckboxOption,
+    selectedCheckboxChoices,
+    choiceIndex
+}) => {
+    const currentChoice = selectedCheckboxChoices.filter(
+        (choice, index) => choice.choiceIndex === choiceIndex
+    )[0]
+
     return (
         <div className="my-3">
             <div
@@ -7,16 +18,35 @@ const ChoiceOptionDetails = ({ choice, option, handleRadioOption, handleCheckbox
                 <input
                     onChange={(event) => {
                         if (choice.type === 'radio') {
-                            handleRadioOption(index, choice.title, option.option_name)
+                            handleRadioOption(
+                                choiceIndex,
+                                choice.title,
+                                option.option_name,
+                                choice.select_count
+                            )
                         }
 
                         if (choice.type === 'checkbox') {
-                            handleCheckboxOption(index, choice.title, option.option_name, event)
+                            handleCheckboxOption(
+                                choiceIndex,
+                                choice.title,
+                                option.option_name,
+                                event,
+                                choice.select_count
+                            )
                         }
                     }}
+                    disabled={
+                        (currentChoice
+                            && !currentChoice.selectedOption.includes(option.option_name)
+                            && currentChoice.select_count === currentChoice.selectedOption.length)
+                            ? true : false
+                    }
+
                     value={option.option_name}
                     type={choice.type}
-                    name={choice.title} className="w-4 accent-gray-800" />
+                    name={choice.title}
+                    className="w-4 accent-gray-800" />
             </div>
             <p className="text-xs font-semibold text-gray-600">
                 + ₱ {option.option_price.toLocaleString()}
