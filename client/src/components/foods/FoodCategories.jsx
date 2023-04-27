8394da28eee452cda0c070d222bd43f66ec455ab
cur1ousFranz <xfranjeff@gmail.com>
2023-04-27 22:57:48 +0800

    change background hover

--- client/src/components/foods/FoodCategories.jsx
+++ client/src/components/foods/FoodCategories.jsx
@@ -8,11 +8,20 @@ const FoodCategories = ({ categories, selectCategory, currentFoodCategory }) => 
     return (
         <div>
             <div className="my-4 space-y-4">
-                <p onClick={() => handleClick('All')} className={currentFoodCategory === 'All' ? "p-2 rounded-md cursor-pointer bg-orange-200" : "p-2 rounded-md cursor-pointer text-gray-700 hover:text-gray-900 hover:bg-orange-200"}>
+                <p
+                    onClick={() => handleClick('All')}
+                    className={currentFoodCategory === 'All'
+                        ? "p-2 rounded-sm cursor-pointer bg-orange-200"
+                        : "p-2 rounded-sm cursor-pointer text-gray-700 hover:text-gray-900 hover:bg-orange-200"}>
                     All
                 </p>
                 {categories.map(category => (
-                    <p onClick={() => handleClick(category.category_name)} className={currentFoodCategory === category.category_name ? "p-2 rounded-md cursor-pointer bg-orange-200" : "p-2 rounded-md cursor-pointer text-gray-700 hover:text-gray-900 hover:bg-orange-200"} key={category._id}>
+                    <p
+                        onClick={() => handleClick(category.category_name)}
+                        className={currentFoodCategory === category.category_name
+                            ? "p-2 rounded-sm cursor-pointer bg-orange-300"
+                            : "p-2 rounded-sm cursor-pointer text-gray-700 hover:text-gray-900 hover:bg-orange-200"}
+                        key={category._id}>
                         {category.category_name}
                     </p>
                 ))}
