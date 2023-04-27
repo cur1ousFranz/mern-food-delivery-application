0f9203a69aeceef6916d9355de6efaed0f54046d
cur1ousFranz <xfranjeff@gmail.com>
2023-04-27 22:52:04 +0800

    public the route

--- server/routes/store.js
+++ server/routes/store.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { getAllStores, storeStore, getStoreDetails } = require('../controllers/storeController')
-const requireAuth = require('../middleware/requireAuth')
 
 router.get('/api/stores', getAllStores)
 router.post('/api/stores', storeStore)
-
-// AUTH MIDDLEWARE
-router.use(requireAuth)
 
 router.get('/api/stores/:id', getStoreDetails)
 
