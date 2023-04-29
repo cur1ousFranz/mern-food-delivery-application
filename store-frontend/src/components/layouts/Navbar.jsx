44fe9e74c741f2ef36a32a36983abcfbafb6b8c9
cur1ousFranz <xfranjeff@gmail.com>
2023-04-30 01:11:05 +0800

    add logout function | layout html  elements

--- store-frontend/src/components/layouts/Navbar.jsx
+++ store-frontend/src/components/layouts/Navbar.jsx
@@ -3,29 +3,44 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
-    const { store } = useContext(AuthContext)
+    const { store, dispatch } = useContext(AuthContext)
     const [profileDropdown, setProfileDropdown] = useState(false)
-    
-    const signoutUser = () => {
 
+    const signoutUser = () => {
+        dispatch({ type: 'LOGOUT' })
+        localStorage.removeItem('store')
     }
 
     return (
         <nav className="py-4 px-6 border-b flex justify-between">
-            <h1 className="text-lg">STORE FRONTEND</h1>
+            <Link to={'/store'} className="text-lg">STORE FRONTEND</Link>
             <div>
                 {store && (
                     <div className="relative">
-                        <img onClick={() => setProfileDropdown(!profileDropdown)} src="person-circle.svg" className="inline-block w-6 cursor-pointer" alt="" />
+                        <img
+                            onClick={() => setProfileDropdown(!profileDropdown)}
+                            src="person-circle.svg"
+                            className="inline-block w-6 cursor-pointer"
+                            alt="" />
 
                         {profileDropdown && (
-                            <div className="origin-top-right absolute right-0 mr-4 mt-2 w-44 z-10 rounded-md shadow-lg">
+                            <div
+                                className="origin-top-right absolute right-0 mr-2 mt-1 w-44 z-10 rounded-md shadow-lg">
                                 <div className="rounded-md border shadow-xs text-sm bg-white">
-                                    <div className="p-2 hover:bg-gray-100">
-                                        <button onClick={signoutUser} className="w-full text-left px-4">Profile</button>
+                                    <div className="p-2 w-full hover:bg-gray-100">
+                                        <Link
+                                            onClick={() => setProfileDropdown(false)}
+                                            to={'/profile'}
+                                            className="w-full text-left px-4">
+                                            Profile
+                                        </Link>
                                     </div>
                                     <div className="p-2 hover:bg-gray-100">
-                                        <button onClick={signoutUser} className="w-full text-left px-4">Sign out</button>
+                                        <button
+                                            onClick={signoutUser}
+                                            className="w-full text-left px-4">
+                                            Sign out
+                                        </button>
                                     </div>
                                 </div>
                             </div>
