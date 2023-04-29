a243ba0b592d67528657d0b4773daa67a217336a
cur1ousFranz <xfranjeff@gmail.com>
2023-04-30 01:06:50 +0800

    register new store route for image update

--- server/routes/store.js
+++ server/routes/store.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const { getAllStores, storeStore, getStoreDetails } = require('../controllers/storeController')
+const {
+    getAllStores,
+    storeStore,
+    getStoreDetails,
+    updateStoreImage
+} = require('../controllers/storeController')
 
 router.get('/api/stores', getAllStores)
 router.post('/api/stores', storeStore)
 
 router.get('/api/stores/:id', getStoreDetails)
+router.put('/api/stores/:id/image', updateStoreImage)
 
 module.exports = router
