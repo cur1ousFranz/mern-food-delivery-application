0b0a76bb6d623eaa32286d6381964f4b13225116
cur1ousFranz <xfranjeff@gmail.com>
2023-05-01 00:25:30 +0800

    reusable function for uppercase word

---
+++ store-frontend/src/toUpperCaseWord.js
@@ -0,0 +1,10 @@
+const toUpperCaseWords = (name) => {
+    let words = name.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+    }
+    let result = words.join(" ");
+    return result
+}
+
+export default toUpperCaseWords
