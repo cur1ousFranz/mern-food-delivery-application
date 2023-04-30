bb4dc333e4b5abf066d8e30d7df093f84e0cdb7b
cur1ousFranz <xfranjeff@gmail.com>
2023-05-01 00:29:06 +0800

    remove db transaction doesnt work, manuall delete user if storing store is failed

--- server/controllers/storeController.js
+++ server/controllers/storeController.js
@@ -4,7 +4,6 @@ const createToken = require('../auth/createToken')
 const { isValidObjectId } = require('mongoose')
 const multer = require('multer')
 const path = require('path')
-const mongoose = require('mongoose')
 
 const getAllStores = async (req, res) => {
     const stores = await Store.find()
@@ -18,11 +17,34 @@ const storeStore = async (req, res) => {
     const role = 'store'
     const verified = false
 
-    try {
-        await mongoose.startSession()
-        const session = await mongoose.startSession()
-        session.startTransaction()
+    const errorFields = []
+    let errorMessage = 'Please fill in all fields.'
+    if (!store_address) {
+        errorFields.push('store_address')
+    }
+    if (!store_name) {
+        errorFields.push('store_name')
+    }
+    if (!brand_name) {
+        errorFields.push('brand_name')
+    }
+    if (!business_type) {
+        errorFields.push('business_type')
+    }
+    if (!first_name) {
+        errorFields.push('first_name')
+    }
+    if (!last_name) {
+        errorFields.push('last_name')
+    }
+    if (!contact_number) {
+        errorFields.push('contact_number')
+    }
+    if (errorFields.length > 0) {
+        return res.status(400).json({ error: errorMessage, errorFields })
+    }
 
+    try {
         const user = await User.signup(email, password, role)
         const store = await Store.create({
             user_id: user._id,
@@ -38,12 +60,10 @@ const storeStore = async (req, res) => {
         })
 
         const token = createToken(user._id)
-        await session.commitTransaction()
-        session.endSession()
-
         res.status(200).json({ email, token, is_role: user.role, id: store._id })
 
     } catch (error) {
+        await User.deleteOne({ email })
         res.status(500).json({ error: error.message })
     }
 }
@@ -64,6 +84,25 @@ const getStoreDetails = async (req, res) => {
     res.status(200).json(store)
 }
 
+const updateStore = async (req, res) => {
+    const { id } = req.params
+    const updated = req.body
+
+    if (!isValidObjectId(id)) {
+        return res.status(404).json({ error: "No such store" })
+    }
+
+    await Store.findByIdAndUpdate({ _id: id }, { ...updated })
+
+    const store = await Store.findById({ _id: id })
+
+    if (!store) {
+        return res.status(400).json({ error: "No such store" })
+    }
+
+    res.status(200).json(store)
+}
+
 const updateStoreImage = async (req, res) => {
 
     try {
@@ -74,13 +113,13 @@ const updateStoreImage = async (req, res) => {
                 const { id } = req.params
                 const filename = req.file.filename
 
-                await Store.findByIdAndUpdate({ _id: id}, {
+                await Store.findByIdAndUpdate({ _id: id }, {
                     image: 'uploads/store/' + filename
                 })
 
-                const store = await Store.findById({ _id: id})
+                const store = await Store.findById({ _id: id })
 
-                if(!store) {
+                if (!store) {
                     return res.status(400).json({ error: 'No such store' })
                 }
 
@@ -133,5 +172,6 @@ module.exports = {
     getAllStores,
     storeStore,
     getStoreDetails,
+    updateStore,
     updateStoreImage
 }
