8a40f2c94554120b726b13ea336b0de5858882fc
cur1ousFranz <xfranjeff@gmail.com>
2023-05-14 19:01:25 +0800

    update token expiry time

--- server/auth/createToken.js
+++ server/auth/createToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 const createToken = (_id) => {
-    return jwt.sign({ _id }, process.env.SECRET_KEY, { expiresIn : '3d' })
+    return jwt.sign({ _id }, process.env.SECRET_KEY, { expiresIn : '3h' })
 }
 
 module.exports = createToken
