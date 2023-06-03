18ae7dfd0f2a65ede8ddceb778cabe9d302435f3
cur1ousFranz <xfranjeff@gmail.com>
2023-06-03 23:06:14 +0800

    register removing order when update status

--- store-frontend/src/reducer/orderReducer.jsx
+++ store-frontend/src/reducer/orderReducer.jsx
@@ -1,16 +1,20 @@
 const orderReducer = (state, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case 'SET_ORDERS':
             return {
-                orders : [...action.payload]
+                orders: [...action.payload]
             }
         case 'ADD_ORDER':
             return {
-                orders : [action.payload, ...state.orders]
+                orders: [action.payload, ...state.orders]
+            }
+        case 'REMOVE_ORDER':
+            return {
+                orders: state.orders.filter(order => order._id !== action.payload._id)
             }
         default:
             return state
     }
 }
- 
+
 export default orderReducer;
